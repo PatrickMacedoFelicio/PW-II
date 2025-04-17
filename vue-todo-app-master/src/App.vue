997edcd7e799cfee3a62eba3454f6cef9b9d3bbc
Patrick Macêdo <patrickmacedofeliceo@gmail.com>
2025-04-16 21:58:20 -0400

    UP

---
+++ vue-todo-app-master/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <IconSvg />
+  <MudarTema />
+  <HeaderNavbar />
+  
+  <main class="flex-shrink-0">
+    <div class="container">
+      <RouterView />
+    </div>
+  </main>
+
+  <FooterBar />
+</template>
+
+<script setup lang="ts">
+import { RouterView } from "vue-router";
+
+import IconSvg from "./components/icons/IconSVG.vue";
+import MudarTema from "./components/layouts/MudarTema.vue";
+import HeaderNavbar from "./components/layouts/HeaderNavbar.vue";
+import FooterBar from "./components/layouts/FooterBar.vue";
+</script>
